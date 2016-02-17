40fdbc5c6cfe00bdd6b4a77d1e54b87b6b8c9217
Mike Lewis <mike_lewis_@hotmail.com>
2016-02-17 12:29:39 -0800

    fixed styling problems

--- application/views/justone.php
+++ application/views/justone.php
@@ -3,13 +3,6 @@
     <div class="span8 offset1">
         <p class="lead">{what}</p><br/>
         <p class="text-right">{who}</p>
-    </div>
-</div>
-< class="row">
-    <div class="span3"><img src="/data/{mug}" titles="{who}"/></div>
-    <div class="span8 offset1">
-        <p class="lead">{what}</p><br/>
-        <p class="text-right">{who}</p>
         <div class="hollywood" data-average="{average}" data-id="{id}"></div>
     </div>
 </div>
